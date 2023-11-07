7b4e385753cc9c965035d25f17ab9103b61540ec
Negrescu29 <ion.negrescu94@gmail.com>
2023-11-07 22:08:11 +0100

    created sale ad page

---
+++ tests/pages/saleAdPage.ts
@@ -0,0 +1,21 @@
+import { type Locator, type Page } from '@playwright/test';
+
+export class SaleAdPage {
+    readonly page: Page;
+    readonly keyword: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.keyword = page.locator('text=garage');
+    }
+
+    /**
+     * this method takes a string as a parameter that will 
+     * represent the searched keyword in the filters modal
+     * @param keyword 
+     */
+    async expectKeywordVisible(keyword: string) {
+        await this.keyword.isVisible();
+
+    }
+}
