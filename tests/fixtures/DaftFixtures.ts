07cb5879e5f2443a1d6a16bd8d1bda3a8d2390f3
Negrescu29 <ion.negrescu94@gmail.com>
2023-11-08 03:21:04 +0100

    trying to import HomePage

---
+++ tests/fixtures/DaftFixtures.ts
@@ -0,0 +1,9 @@
+import { ResultsPage } from '../pages/resultsPage';
+import { SaleAdPage } from '../pages/saleAdPage';
+import { HomePage } from '../pages/homePage';
+
+export type DaftFixtures = {
+    homePage: HomePage;
+    resultsPage: ResultsPage;
+    saleAdPage: SaleAdPage;
+};
