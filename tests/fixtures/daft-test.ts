3dd006753211498791ffe7eaf4d13ca5815f019c
Negrescu29 <ion.negrescu94@gmail.com>
2023-11-08 03:10:33 +0100

    re-importing HomePage class

--- tests/fixtures/daft-test.ts
+++ tests/fixtures/daft-test.ts
@@ -1,27 +1,27 @@
 import { test as base } from '@playwright/test';
-import { HomePage } from '../pages/homePage';
 import { ResultsPage } from '../pages/resultsPage';
 import { SaleAdPage } from '../pages/saleAdPage';
+import { HomePage } from '../pages/homePage';
 
 type DaftFixtures = {
-  homePage: HomePage;
-  resultsPage: ResultsPage;
-  saleAdPage: SaleAdPage;
+    homePage: HomePage;
+    resultsPage: ResultsPage;
+    saleAdPage: SaleAdPage;
 };
 
 export const test = base.extend<DaftFixtures>({
 
-  homePage: async ({ page }, use) => {
-    await use(new HomePage(page));
-  },
+    homePage: async ({ page }, use) => {
+        await use(new HomePage(page));
+    },
 
-  resultsPage: async ({ page }, use) => {
-    await use(new ResultsPage(page));
-  },
+    resultsPage: async ({ page }, use) => {
+        await use(new ResultsPage(page));
+    },
 
-  saleAdPage: async ({ page }, use) => {
-    await use(new SaleAdPage(page));
-  },
+    saleAdPage: async ({ page }, use) => {
+        await use(new SaleAdPage(page));
+    },
 });
 
 export { expect } from '@playwright/test';
