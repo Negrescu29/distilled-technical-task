e74439018a1d6c53c1752c7d4cf47dbebe17ae83
Negrescu29 <ion.negrescu94@gmail.com>
2023-11-08 02:55:26 +0100

    keyword filtering test

--- tests/keyword-filtering.spec.ts
+++ tests/keyword-filtering.spec.ts
@@ -1,34 +1,39 @@
-import { test, expect } from '@playwright/test';
-import { HomePage } from './pages/homePage';
-import { ResultsPage } from './pages/resultsPage';
-import { SaleAdPage } from './pages/saleAdPage';
+import { test, expect } from './fixtures/daft-test';
+
+test.describe('Keyword filtering on daft website', () => {
+    const location = 'Dublin County';
+    const results = '3,042 Properties for Sale in Dublin (County)';
+    const resultsWithKeyword = '402 Properties for Sale in Dublin (County)';
 
 
-test('Keyword filtering', async ({ page }) => {
+    test('Check keyword filtered sell ads are return in the result list', async (
+        { homePage, resultsPage, saleAdPage }) => {
 
-    // construct each page object 
-    const homePage = new HomePage(page);
-    const resultsPage = new ResultsPage(page);
-    const saleAdPage = new SaleAdPage(page);
+        // load the website
+        await homePage.load();
 
-    // load the website
-    await homePage.load();
+        // search for Dublin County
+        await homePage.searchForLocation(location);
 
-    // search for Dublin County
-    await homePage.searchForLocation('Dublin County')
+        // check that there are results for this location
+        await resultsPage.expectResultsForThisLocation(results);
 
-    // check that there are results for this location
-    await resultsPage.expectResultsForThisLocation('3,042 Properties for Sale in Dublin (County)');
+        // filter results using Garage keyword
+        await resultsPage.searchForKeyword('garage');
 
-    // filter results using Garage keyword
-    await resultsPage.searchForKeyword('garage');
+        // check that there are results for this keyword
+        await resultsPage.expectResultsForThisLocation(resultsWithKeyword);
 
-    // check that there are results for this keyword
-    await resultsPage.expectResultsForThisLocation('402 Properties for Sale in Dublin (County)')
+        // open first ad from the results list
+        await resultsPage.clickOnFirstResult();
 
-    // open first ad from the results list
-    await resultsPage.tapOnFirstResult();
+        // check for `garage` on sale ad page
+        await saleAdPage.expectKeywordVisible('garage');
+    });
 
-    // check for `garage` on sale ad page
-    await saleAdPage.expectKeywordVisible('garage');
 });
+
+
+
+
+
