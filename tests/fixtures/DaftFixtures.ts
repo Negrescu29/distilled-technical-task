8addf7c869ea7773f5198da5ddc0b3f1f4cbca26
Negrescu29 <ion.negrescu94@gmail.com>
2023-11-08 03:24:56 +0100

    revert latest changes

--- tests/fixtures/DaftFixtures.ts
+++
@@ -1,9 +0,0 @@
-import { ResultsPage } from '../pages/resultsPage';
-import { SaleAdPage } from '../pages/saleAdPage';
-import { HomePage } from '../pages/homePage';
-
-export type DaftFixtures = {
-    homePage: HomePage;
-    resultsPage: ResultsPage;
-    saleAdPage: SaleAdPage;
-};
